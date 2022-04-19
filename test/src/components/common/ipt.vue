9966c3038b2ac0d1156e6b60860a85ee76e513a5
Apprenhene <3514392356@qq.com>
2022-04-19 10:36:10 +0800

    报名页面的一个案例

---
+++ test/src/components/common/ipt.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper">
+    <div class="input-data">
+      <input
+      :type="type"
+      required
+      :rule="rule"
+      v-model="content"
+      >
+      <div class="underline"></div>
+      <label>{{label}}</label>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ipt',
+  props: ['label', 'type', 'rule']
+}
+</script>
+
+<style>
+  .wrapper{
+    width: 450px;
+    background-color: #fff;
+    padding: 40px;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+    border-radius: 8px;
+}
+.wrapper .input-data{
+    position:relative;
+    width: 100%;
+    height: 40px;
+}
+.wrapper .input-data input{
+    width: 100%;
+    height: 100%;
+    border:none;
+    outline: none;
+    font-size: 17px;
+    border-bottom: 2px solid #c0c0c0;
+}
+.wrapper .input-data input:focus ~ label,
+.wrapper .input-data input:valid ~ label{
+    transform: translateY(-25px);
+    font-size: 15px;
+    color: black;
+    font-weight: bold;
+}
+.wrapper .input-data label{
+    position: absolute;
+    bottom: 10px;
+    left: 0px;
+    color: #808080;
+    pointer-events: none;
+    transition: all 0.3s ease;
+}
+.wrapper .input-data .underline{
+    position: absolute;
+    bottom: 0px;
+    height: 2px;
+    width: 100%;
+    background-color: #2c6fdb;
+    transform: scaleX(0);
+    transition: all 0.3s ease;
+}
+.wrapper .input-data input:focus ~ .underline,
+.wrapper .input-data input:valid ~ .underline{
+    transform: scaleX(1);
+}
+</style>
--- test/src/components/view/Login.vue
+++ test/src/components/view/Login.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="login">
-    111
+    <ipt
+      label="姓名"
+      type="text"
+      rule="^.{6,16}$"
+      style="margin:50px auto"
+    />
   </div>
 </template>
 
 <script>
+import ipt from '../common/ipt.vue'
 export default {
+  components: { ipt },
   name: 'login'
 }
 </script>
