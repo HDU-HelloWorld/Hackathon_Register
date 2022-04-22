9285d2140b68b46e473aec0d9aa3c4d1a20ac144
HDU_Nbsp <1808347356@qq.com>
2022-04-22 11:12:37 +0800

    重构了表单页面，引入了Element UI组件库

--- test/src/components/view/Login.vue
+++ test/src/components/view/Login.vue
@@ -1,79 +1,42 @@
 <template>
   <div class="login">
     <div class="wrapper">
-      <div class="floor">
-        <ipt
-          label="姓名"
-          type="text"
-          rule="^.{2,10}$"
-          style="margin:25px 10px "
-          @inputChange="res => model.name = res "
-          />
-        <ipt
-          label="年龄"
-          type="text"
-          rule="^-?\d+$ "
-          style="margin:25px 10px "
-          @inputChange="res => model.age = res "
-          />
-      </div>
-      <ipt
-          label="电话"
-          type="text"
-          rule="^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$"
-          style="margin:25px 10px "
-          @inputChange="res => model.phone = res "
-      />
-      <ipt
-          label="邮箱"
-          type="text"
-          rule="^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"
-          style="margin:25px 10px "
-          @inputChange="res => model.email = res "
-      />
-      <div class="floor">
-        <ipt
-          label="所在学院"
-          type="text"
-          rule="^[\u4e00-\u9fa5],{0,}$ "
-          style="margin:25px 10px "
-          @inputChange="res => model.college = res "
-        />
-        <ipt
-          label="所在年级"
-          type="text"
-          rule="^[\u4e00-\u9fa5],{0,}$ "
-          style="margin:25px 10px "
-          @inputChange="res => model.grader = res "
-        />
-      </div>
-      <div class="floor">
-        <ipt
-          label="是否组队"
-          type="checkbox"
-          rule="^.{6,16}$"
-          style="margin:25px 10px "
-        />
-        <ipt
-          label="队名"
-          type="text"
-          rule="^.{6,16}$"
-          style="margin:25px 10px "
-          v-show="panduan"
-          @inputChange="res => model.teamname = res "
-        />
-      </div>
-      <div class="floor">
-        <ipt
-          label="你的个人介绍"
-          type="text"
-          rule="^.{0,500}$"
-          style="margin:25px 10px "
-          @inputChange="res => model.intro = res "
-        />
-      </div>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="姓名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄">
+          <el-input v-model="form.age"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="form.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="form.email"></el-input>
+        </el-form-item>
+        <div class="floor">
+          <el-form-item label="所在学院">
+            <el-input v-model="form.college"></el-input>
+          </el-form-item>
+          <el-form-item label="所在年级">
+            <el-input v-model="form.grader"></el-input>
+          </el-form-item>
+        </div>
+        <el-form-item label="是否组队" class="team">
+          <el-checkbox v-model="form.team" name="type"></el-checkbox>
+          <el-form-item label="队名" v-if="form.team">
+            <el-input v-model="form.teamname"></el-input>
+          </el-form-item>
+        </el-form-item>
+        <el-form-item label="个人介绍">
+          <el-input type="textarea" v-model="form.intro"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">提交</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
     </div>
-    <button @click="submit">提交捏</button>
   </div>
 </template>
 
@@ -85,13 +48,14 @@ export default {
   data () {
     return {
       panduan: '',
-      model: {
+      form: {
         name: '',
         age: '',
         phone: '',
         email: '',
         college: '',
         grader: '',
+        team: true,
         teamname: '',
         intro: ''
       }
@@ -103,47 +67,29 @@ export default {
     })
   },
   methods: {
-    submit () {
-      let rulg = /^.{6,16}/
-      if (
-        rulg.test(this.model.name) &&
-        rulg.test(this.model.age) &&
-        rulg.test(this.model.phone) &&
-        rulg.test(this.model.email) &&
-        rulg.test(this.model.college) &&
-        rulg.test(this.model.grader) &&
-        rulg.test(this.model.teamname) &&
-        rulg.test(this.model.intro)
-      ) {
-        alert('注册成功')
-        console.log(this.model)
-      } else {
-        alert('注册失败')
-      }
-    }
+    onSubmit () {}
   }
 }
 
 </script>
 
-<style>
-.login{
+<style scoped>
+.login {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
 }
-.wrapper{
-    display: flex;
-    flex-direction: column;
-    width: 450px;
-    background-color: #fff;
-    padding: 25px;
-    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
-    border-radius: 8px;
-    margin:25px auto;
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 450px;
+  background-color: #fff;
+  padding: 25px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  margin: 25px auto;
 }
-.floor{
+.floor {
   display: flex;
 }
-
 </style>
